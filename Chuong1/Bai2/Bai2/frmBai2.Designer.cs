ea2a0fd21013d6ccaf6c1bda3af3b88db4d463f0
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-03-03 22:16:32 +0700

    Bai2

---
+++ Chuong1/Bai2/Bai2/frmBai2.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Bai2
+{
+    partial class frmBai2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBanTinRo = new System.Windows.Forms.Label();
+            this.lblBanTinDuocMaHoa = new System.Windows.Forms.Label();
+            this.lblBanTinDuocGiaiMa = new System.Windows.Forms.Label();
+            this.txtBanTinRo = new System.Windows.Forms.TextBox();
+            this.txtBanTinDuocMaHoa = new System.Windows.Forms.TextBox();
+            this.txtBanTinDuocGiaiMa = new System.Windows.Forms.TextBox();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.btnGiaiMa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBanTinRo
+            // 
+            this.lblBanTinRo.AutoSize = true;
+            this.lblBanTinRo.Location = new System.Drawing.Point(69, 51);
+            this.lblBanTinRo.Name = "lblBanTinRo";
+            this.lblBanTinRo.Size = new System.Drawing.Size(52, 13);
+            this.lblBanTinRo.TabIndex = 0;
+            this.lblBanTinRo.Text = "Bản tin rõ";
+            // 
+            // lblBanTinDuocMaHoa
+            // 
+            this.lblBanTinDuocMaHoa.AutoSize = true;
+            this.lblBanTinDuocMaHoa.Location = new System.Drawing.Point(69, 104);
+            this.lblBanTinDuocMaHoa.Name = "lblBanTinDuocMaHoa";
+            this.lblBanTinDuocMaHoa.Size = new System.Drawing.Size(106, 13);
+            this.lblBanTinDuocMaHoa.TabIndex = 1;
+            this.lblBanTinDuocMaHoa.Text = "Bản tin được mã hóa";
+            // 
+            // lblBanTinDuocGiaiMa
+            // 
+            this.lblBanTinDuocGiaiMa.AutoSize = true;
+            this.lblBanTinDuocGiaiMa.Location = new System.Drawing.Point(69, 153);
+            this.lblBanTinDuocGiaiMa.Name = "lblBanTinDuocGiaiMa";
+            this.lblBanTinDuocGiaiMa.Size = new System.Drawing.Size(104, 13);
+            this.lblBanTinDuocGiaiMa.TabIndex = 2;
+            this.lblBanTinDuocGiaiMa.Text = "Bản tin được giải mã";
+            // 
+            // txtBanTinRo
+            // 
+            this.txtBanTinRo.Location = new System.Drawing.Point(188, 48);
+            this.txtBanTinRo.Name = "txtBanTinRo";
+            this.txtBanTinRo.Size = new System.Drawing.Size(216, 20);
+            this.txtBanTinRo.TabIndex = 3;
+            this.txtBanTinRo.TextChanged += new System.EventHandler(this.txtBanTinRo_TextChanged);
+            // 
+            // txtBanTinDuocMaHoa
+            // 
+            this.txtBanTinDuocMaHoa.Location = new System.Drawing.Point(188, 97);
+            this.txtBanTinDuocMaHoa.Name = "txtBanTinDuocMaHoa";
+            this.txtBanTinDuocMaHoa.Size = new System.Drawing.Size(216, 20);
+            this.txtBanTinDuocMaHoa.TabIndex = 4;
+            this.txtBanTinDuocMaHoa.TextChanged += new System.EventHandler(this.txtBanTinDuocMaHoa_TextChanged);
+            // 
+            // txtBanTinDuocGiaiMa
+            // 
+            this.txtBanTinDuocGiaiMa.Location = new System.Drawing.Point(188, 153);
+            this.txtBanTinDuocGiaiMa.Name = "txtBanTinDuocGiaiMa";
+            this.txtBanTinDuocGiaiMa.Size = new System.Drawing.Size(216, 20);
+            this.txtBanTinDuocGiaiMa.TabIndex = 5;
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Enabled = false;
+            this.btnMaHoa.Location = new System.Drawing.Point(130, 230);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(75, 23);
+            this.btnMaHoa.TabIndex = 6;
+            this.btnMaHoa.Text = "Mã Hóa";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.btnMaHoa_Click);
+            // 
+            // btnGiaiMa
+            // 
+            this.btnGiaiMa.Enabled = false;
+            this.btnGiaiMa.Location = new System.Drawing.Point(301, 230);
+            this.btnGiaiMa.Name = "btnGiaiMa";
+            this.btnGiaiMa.Size = new System.Drawing.Size(75, 23);
+            this.btnGiaiMa.TabIndex = 7;
+            this.btnGiaiMa.Text = "Giải mã";
+            this.btnGiaiMa.UseVisualStyleBackColor = true;
+            this.btnGiaiMa.Click += new System.EventHandler(this.btnGiaiMa_Click);
+            // 
+            // frmBai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 334);
+            this.Controls.Add(this.btnGiaiMa);
+            this.Controls.Add(this.btnMaHoa);
+            this.Controls.Add(this.txtBanTinDuocGiaiMa);
+            this.Controls.Add(this.txtBanTinDuocMaHoa);
+            this.Controls.Add(this.txtBanTinRo);
+            this.Controls.Add(this.lblBanTinDuocGiaiMa);
+            this.Controls.Add(this.lblBanTinDuocMaHoa);
+            this.Controls.Add(this.lblBanTinRo);
+            this.Name = "frmBai2";
+            this.Text = "Bài Toán 2";
+            this.Load += new System.EventHandler(this.frmBai2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBanTinRo;
+        private System.Windows.Forms.Label lblBanTinDuocMaHoa;
+        private System.Windows.Forms.Label lblBanTinDuocGiaiMa;
+        private System.Windows.Forms.TextBox txtBanTinRo;
+        private System.Windows.Forms.TextBox txtBanTinDuocMaHoa;
+        private System.Windows.Forms.TextBox txtBanTinDuocGiaiMa;
+        private System.Windows.Forms.Button btnMaHoa;
+        private System.Windows.Forms.Button btnGiaiMa;
+    }
+}
+
---
+++ Chuong1/Bai2/Bai2/frmBai2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBai2_Load(object sender, EventArgs e)
+        {
+            TaoBangHoanVi(); string s = "";
+            for (int i = 0; i < 26; i++) s += BangHoanVi[i];
+            MessageBox.Show(s, "Bảng hoán vị ngẫu nhiên");
+        }
+        char[] BangHoanVi = new char[26];
+        private void TaoBangHoanVi()
+        {
+            for (int i = 0; i < 26; i++) BangHoanVi[i] = (char)(65 + i);
+            Random RndObj = new Random(DateTime.Now.Millisecond);
+            int SoLan = 20 + RndObj.Next(80); for (int n = 0; n < SoLan; n++)
+            {
+                int n1 = RndObj.Next(26); int n2 = RndObj.Next(26);
+                char c = BangHoanVi[n1]; BangHoanVi[n1] = BangHoanVi[n2]; BangHoanVi[n2] = c;
+            }
+        }
+        private bool KiemTraHopLe(string s)
+        {
+            bool HopLe = true;
+            for (int i = 0; i < s.Length; i++)
+            { char c = s[i]; if (c < 'A' || c > 'Z')
+                {
+                    HopLe = false;
+                    break;
+                }
+            }
+            return HopLe;
+        }
+        private int TimViTri(char c)
+        {
+            return c - 65;
+        }
+        private int TimViTriNguoc(char c)
+        {
+            for (int i = 0; i < 26; i++)
+                if (BangHoanVi[i] == c) return i;
+            return -1; // Báo sai
+        }
+
+        private void txtBanTinRo_TextChanged(object sender, EventArgs e)
+        {
+            btnMaHoa.Enabled = KiemTraHopLe(txtBanTinRo.Text);
+        }
+
+        private void txtBanTinDuocMaHoa_TextChanged(object sender, EventArgs e)
+        {
+            btnGiaiMa.Enabled = KiemTraHopLe(txtBanTinDuocMaHoa.Text);
+        }
+
+        private void btnMaHoa_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            for (int i = 0; i < txtBanTinRo.Text.Length; i++)
+            {
+                int vitri = TimViTri(txtBanTinRo.Text[i]);
+                s += BangHoanVi[vitri];
+            }
+            txtBanTinDuocMaHoa.Text = s;
+        }
+
+        private void btnGiaiMa_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            for (int i = 0; i < txtBanTinRo.Text.Length; i++)
+            {
+                int vitri = TimViTriNguoc(txtBanTinDuocMaHoa.Text[i]);
+                s += (char)(vitri+65);
+            }
+            txtBanTinDuocGiaiMa.Text = s;
+        }
+    }
+}
