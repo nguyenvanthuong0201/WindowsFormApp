88500e930b90556721a7518828c50e8ecc4eb0b6
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-03-18 23:39:15 +0700

    70%

---
+++ Tuần 3/MaHoaDonGian/MaHoaDonGian/MaHoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaHoaDonGian
+{
+    class MaHoa
+    {
+        CommonProcess cProcess = null; 
+    }
+    public MaHoa(int algorithm_number, frmMaHoaGiaiMa.ProgressEventHandler IncrementProgress, frmMaHoaGiaiMa.ProgressInitHandler InitProgress)
+    {
+        if (CacGiaiThuat.DES == algorithm_number || CacGiaiThuat.DES_File == algorithm_number)
+        {
+            cProcess = new DES.ProcessDES(IncrementProgress, InitProgress);
+        }
+        else
+        {
+            cProcess = new AES.ProcessAES(IncrementProgress, InitProgress);
+        }
+    }
+
+}
---
+++ Tuần 3/MaHoaDonGian/MaHoaDonGian/ProgressEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaHoaDonGian
+{
+    public class ProgressEventArgs : EventArgs
+    {
+        public int Increment;
+        public ProgressEventArgs(int Inc)
+        {
+            this.Increment = Inc;
+        }
+    }
+    public class ProgressInitArgs : EventArgs
+    {
+        public int Maximum;
+        public ProgressInitArgs(int Max)
+        {
+            this.Maximum = Max;
+        }
+    }
+}
